c3be33f1c03c8f7497e0f9b5af27837ecceb55d2
rigen1978 <1768363003@qq.com>
2024-02-15 01:45:24 +0900

    0003:springboot2 ===> springboot application.yml 設定ファイル

---
+++ springboot_02_base_configuration/src/main/java/com/yuan/Demo02Application.java
@@ -0,0 +1,13 @@
+package com.yuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo02Application.class, args);
+	}
+
+}
